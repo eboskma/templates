53c2fad647a8800a5ea3ec10c463cbfcadeff9e1
Erwin Boskma <erwin@datarift.nl>
2022-07-11 17:07:09 +0200

    Rewrite elixir and phoenix templates with numtide/devshell

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -3,25 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }@inputs:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+        };
       in
       {
+        formatter = pkgs.nixpkgs-fmt;
+
         packages = { };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ erlang elixir libnotify inotify-tools ];
-          nativeBuildInputs = [ elixir_ls ];
-        };
-
+        devShells.default =
+          pkgs.devshell.mkShell {
+            imports = [
+              (pkgs.devshell.importTOML ./devshell.toml)
+            ];
+          };
       });
 }
--- phoenix/flake.nix
+++ phoenix/flake.nix
@@ -1,38 +1,38 @@
 {
   description = "A flake for a Phoenix web application";
+  
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }@inputs:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        startdb = pkgs.writeShellApplication {
-          name = "startdb";
-          text = ''
-            ${pkgs.postgresql}/bin/pg_ctl start -l "''${PGDATA:?}/server.log" -o "--unix_socket_directories=''${PGDATA:?}/../run/"
-          '';
-        };
-        stopdb = pkgs.writeShellApplication {
-          name = "stopdb";
-          text = ''
-            ${pkgs.postgresql}/bin/pg_ctl stop
-          '';
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
         };
       in
       {
+        formatter = pkgs.nixpkgs-fmt;
+
         packages = { };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ erlang elixir postgresql ] ++ lib.optionals stdenv.isLinux [ libnotify inotify-tools ];
-          nativeBuildInputs = [ (with nodePackages; pnpm) startdb stopdb nodejs-17_x elixir_ls ];
-        };
-      }
-    );
+        devShells.default =
+          pkgs.devshell.mkShell {
+            imports = [
+              (pkgs.devshell.importTOML ./devshell.toml)
+            ];
+          };
+      });
 }
