52869de0e01e7e82ffcffe29aa94f4aded04b412
Erwin Boskma <erwin@datarift.nl>
2023-03-28 23:08:51 +0200

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,53 +1,61 @@
 {
   description = "A collection of flake templates";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
 
-    templates = {
+      templates = {
 
-      base = {
-        path = ./base;
-        description = "An empty flake with a devShell, useful as a starting point.";
+        base = {
+          path = ./base;
+          description = "An empty flake with a devShell, useful as a starting point.";
+        };
+
+        elixir = {
+          path = ./elixir;
+          description = "A basic flake for elixir development";
+        };
+
+        phoenix = {
+          path = ./phoenix;
+          description = "A flake for developing Phoenix projects";
+        };
+
+        rust = {
+          path = ./rust;
+          description = "A flake for Rust projects";
+        };
+
+        meson = {
+          path = ./meson;
+          description = "A basic flake for projects using meson";
+        };
+
+        lisp = {
+          path = ./lisp;
+          description = "A flake for sbcl";
+        };
+
+        racket = {
+          path = ./racket;
+          description = "A flake with a Racket devshell";
+        };
+
+        livebook = {
+          path = ./livebook;
+          description = "A flake to run Elixir Livebook";
+        };
+
       };
 
-      elixir = {
-        path = ./elixir;
-        description = "A basic flake for elixir development";
-      };
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
 
-      phoenix = {
-        path = ./phoenix;
-        description = "A flake for developing Phoenix projects";
+      devShells.x86_64-linux.default = with pkgs; mkShell {
+        name = "flake-templates";
+        packages = [ taplo ];
       };
-
-      rust = {
-        path = ./rust;
-        description = "A flake for Rust projects";
-      };
-
-      meson = {
-        path = ./meson;
-        description = "A basic flake for projects using meson";
-      };
-
-      lisp = {
-        path = ./lisp;
-        description = "A flake for sbcl";
-      };
-
-      racket = {
-        path = ./racket;
-        description = "A flake with a Racket devshell";
-      };
-
-      livebook = {
-        path = ./livebook;
-        description = "A flake to run Elixir Livebook";
-      };
-
     };
-
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-
-  };
 }
