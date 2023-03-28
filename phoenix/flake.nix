84d11914dafb240ce464a5645e5ef360d9e17a61
Erwin Boskma <erwin@datarift.nl>
2023-03-28 23:09:11 +0200

    Use flake-parts as alternative to devshell.toml

--- phoenix/flake.nix
+++ phoenix/flake.nix
@@ -3,11 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -15,66 +14,104 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, devshell, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlay ];
-        };
-        tailwindcss =
-          let
-            version = "3.1.8";
-          in
-          pkgs.stdenv.mkDerivation {
-            pname = "tailwindcss";
-            inherit version;
-
-            src = builtins.fetchurl {
-              url = "https://github.com/tailwindlabs/tailwindcss/releases/download/v${version}/tailwindcss-linux-x64";
-              sha256 = "0dzk9lm61s78lvcm5bhmi0qmfy5dssac1yrxysf5b99nayna0xzv";
-            };
-
-            dontUnpack = true;
-            dontPatch = true;
-            dontConfigure = true;
-            dontBuild = true;
-            dontFixup = true;
-
-            installPhase = ''
-              install -Dm0755 $src $out/bin/tailwindcss
-            '';
-          };
-      in
+  outputs = { self, nixpkgs, flake-parts, devshell, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; }
       {
-        formatter = pkgs.nixpkgs-fmt;
+        imports = [ devshell.flakeModule ];
 
-        packages = { };
+        systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        devShells.default =
-          pkgs.devshell.mkShell {
-            imports = [
-              (pkgs.devshell.importTOML ./devshell.toml)
-            ];
+        perSystem = { pkgs, ... }:
+          let
+            tailwindcss =
+              let
+                version = "3.1.8";
+              in
+              pkgs.stdenv.mkDerivation {
+                pname = "tailwindcss";
+                inherit version;
 
-            env = [
+                src = builtins.fetchurl {
+                  url = "https://github.com/tailwindlabs/tailwindcss/releases/download/v${version}/tailwindcss-linux-x64";
+                  sha256 = "0dzk9lm61s78lvcm5bhmi0qmfy5dssac1yrxysf5b99nayna0xzv";
+                };
+
+                dontUnpack = true;
+                dontPatch = true;
+                dontConfigure = true;
+                dontBuild = true;
+                dontFixup = true;
+
+                installPhase = ''
+                  install -Dm0755 $src $out/bin/tailwindcss
+                '';
+              };
+          in
+          {
+            formatter = pkgs.nixpkgs-fmt;
+
+            packages = { };
+
+            devshells.default =
               {
-                name = "ESBUILD_PATH";
-                value = "${pkgs.esbuild}/bin/esbuild";
-              }
-              {
-                name = "ESBUILD_VERSION";
-                value = pkgs.esbuild.version;
-              }
-              {
-                name = "TAILWIND_PATH";
-                value = "${tailwindcss}/bin/tailwindcss";
-              }
-              {
-                name = "TAILWIND_VERSION";
-                value = tailwindcss.version;
-              }
-            ];
+                imports = [ "${inputs.devshell}/extra/services/postgres.nix" ];
+
+                packages = with pkgs; [
+                  erlang
+                  libnotify
+                  inotify-tools
+                  nodejs
+                  elixir_ls
+                  gnumake
+                  gcc
+                ];
+
+                services.postgres.initdbArgs = [ "--no-locale" "--encoding=UTF-8" ];
+
+                env = [
+                  {
+                    name = "HEX_HOME";
+                    eval = "$PWD/.nix/hex";
+                  }
+                  {
+                    name = "MIX_HOME";
+                    eval = "$PWD/.nix/mix";
+                  }
+                  {
+                    name = "PATH";
+                    prefix = "$MIX_HOME/escripts";
+                  }
+                  {
+                    name = "ERL_AFLAGS";
+                    value = "-kernel shell_history enabled";
+                  }
+                  {
+                    name = "ESBUILD_PATH";
+                    value = "${pkgs.esbuild}/bin/esbuild";
+                  }
+                  {
+                    name = "ESBUILD_VERSION";
+                    value = pkgs.esbuild.version;
+                  }
+                  {
+                    name = "TAILWIND_PATH";
+                    value = "${tailwindcss}/bin/tailwindcss";
+                  }
+                  {
+                    name = "TAILWIND_VERSION";
+                    value = tailwindcss.version;
+                  }
+                ];
+
+                commands = [
+                  {
+                    name = "mix";
+                    package = pkgs.elixir;
+                    help = "mix";
+                  }
+                ];
+              };
+
           };
-      });
+      };
 }
