55a59a90b0b6be05d6947884a56c9b18f2a3b33f
Erwin Boskma <erwin@datarift.nl>
2023-08-02 13:46:48 +0200

    Refactor Rust template to use flake-parts

--- rust/flake.nix
+++ rust/flake.nix
@@ -3,33 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.rust-overlay.follows = "rust-overlay";
     };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, pre-commit-hooks, crane, flake-compat }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-parts, rust-overlay, pre-commit-hooks, crane, ...}@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { self', pkgs, system, ... }:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
 
         # Use one of these methods to pull in a Rust toolchain
         # Use the default stable Rust
@@ -43,6 +38,8 @@
         #   (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
 
         crane-lib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        pname = "hello-ferris";
+        version = "0.1.0";
         src = crane-lib.cleanCargoSource ./.;
         buildInputs = with pkgs; [
         ] ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
@@ -52,40 +49,32 @@
         ];
 
         cargoArtifacts = crane-lib.buildDepsOnly {
-          inherit src buildInputs nativeBuildInputs;
+          inherit src buildInputs nativeBuildInputs pname version;
         };
 
         hello-ferris = crane-lib.buildPackage {
-          inherit cargoArtifacts src buildInputs nativeBuildInputs;
+          inherit cargoArtifacts src buildInputs nativeBuildInputs pname version;
         };
       in
       {
+        _module.args.pkgs = import nixpkgs { inherit system overlays; };
         formatter = pkgs.nixpkgs-fmt;
 
         packages = {
           inherit hello-ferris;
         };
-        packages.default = self.packages.${system}.hello-ferris;
-
-        apps.hello-ferris = flake-utils.lib.mkApp {
-          drv = self.packages."${system}".default;
-        };
-        apps.default = self.apps.${system}.hello-ferris;
-
-        overlays = final: prev: {
-          hello-ferris = self.packages.${system}.hello-ferris;
-        };
+        packages.default = self'.packages.hello-ferris;
 
         checks = {
           inherit hello-ferris;
 
           hello-ferris-clippy = crane-lib.cargoClippy {
-            inherit cargoArtifacts src buildInputs nativeBuildInputs;
+            inherit cargoArtifacts src buildInputs nativeBuildInputs pname version;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           };
 
           hello-ferris-fmt = crane-lib.cargoFmt {
-            inherit src;
+            inherit src pname version;
           };
 
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
@@ -115,6 +104,6 @@
           packages = [ rustToolchain cargo-edit cargo-diet cargo-feature cargo-outdated pre-commit rust-analyzer ];
         };
 
-      });
-
+      };
+    };
 }
