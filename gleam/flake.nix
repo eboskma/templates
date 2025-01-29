8285f8fa5453d0cd956deb18f678d5ccd582ca1a
Erwin Boskma <erwin@horus.nu>
2025-01-29 20:20:38 +0100

    Add Gleam template

---
+++ gleam/flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "A flake to build Gleam applications";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, config, ... }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
+
+          pre-commit = {
+            settings = {
+              hooks = {
+                nixfmt.enable = true;
+                deadnix.enable = true;
+                gleam.enable = true;
+                taplo.enable = true;
+              };
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              deadnix.enable = true;
+              gleam.enable = true;
+              taplo.enable = true;
+            };
+          };
+
+          devShells.default =
+            with pkgs;
+            mkShell {
+              packages = [
+                gleam
+                erlang_27
+                rebar3
+                taplo
+              ];
+
+              shellHook = ''
+                ${config.pre-commit.pre-commit-check.shellHook}
+              '';
+            };
+        };
+    };
+}
