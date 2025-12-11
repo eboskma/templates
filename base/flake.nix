336931f4d815fc453f6dfbf62f63c3f4ec4f61b1
Erwin Boskma <erwin@horus.nu>
2025-12-11 15:22:44 +0100

    Extend base flake with git-hooks and treefmt

--- base/flake.nix
+++ base/flake.nix
@@ -4,16 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-parts,
-      ...
-    }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "aarch64-linux"
         "aarch64-darwin"
@@ -21,12 +24,41 @@
         "x86_64-linux"
       ];
 
-      perSystem =
-        { pkgs, ... }:
-        {
-          formatter = pkgs.nixpkgs-fmt;
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
 
-          devShells.default = with pkgs; mkShell { };
+      perSystem =
+        { pkgs, config, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.lock";
+
+            programs = {
+              nixfmt.enable = true;
+              deadnix.enable = true;
+              statix.enable = true;
+            };
+          };
+
+          pre-commit = {
+            settings = {
+              hooks = {
+                nixfmt.enable = true;
+                deadnix.enable = true;
+                static.enable = true;
+              };
+            };
+          };
+
+          devShells.default =
+            with pkgs;
+            mkShell {
+              packages = [ ];
+
+              inherit (config.pre-commit) shellHook;
+            };
         };
     };
 }
